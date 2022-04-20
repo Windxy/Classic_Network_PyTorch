7e629e601ebc23494027729fc1a80d163109230d
Small_wind <907799726@qq.com>
2022-04-20 16:32:33 +0800

    Highway Net

---
+++ HighwayNet.py
@@ -0,0 +1,36 @@
+'''HighwayNet''' # ref from: https://github.com/kefirski/pytorch_Highway
+# paper link: https://arxiv.org/abs/1505.00387
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class Highway(nn.Module):
+    def __init__(self, input_size, num_layers, f):
+        super(Highway, self).__init__()
+        size = input_size
+        self.num_layers = num_layers
+        self.nonlinear = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
+        self.linear = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
+        self.gate = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
+        self.f = f
+
+    def forward(self, x):
+        """
+            :param x: tensor with shape of [batch_size, size]
+            :return: tensor with shape of [batch_size, size]
+            applies σ(x) ⨀ (f(G(x))) + (1 - σ(x)) ⨀ (Q(x)) transformation | G and Q is affine transformation,
+            f is non-linear transformation, σ(x) is affine transformation with sigmoid non-linearition
+            and ⨀ is element-wise multiplication
+            """
+        for layer in range(self.num_layers):
+            gate = torch.sigmoid(self.gate[layer](x))   # F.sigmoid 淘汰
+            nonlinear = self.f(self.nonlinear[layer](x))
+            linear = self.linear[layer](x)
+            x = gate * nonlinear + (1 - gate) * linear
+        return x
+
+# 构建网络
+if __name__ == '__main__':
+    net = Highway(input_size=10, num_layers=5, f=F.relu)
+    X = torch.rand(1, 10)   # batch_size, size
+    print(net(X).shape)
